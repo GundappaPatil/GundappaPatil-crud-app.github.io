f030ed5f722131120c4be3a19e5da529610a32b1
Nikhil <nikhilprakashrao@gmail.com>
2022-12-21 15:58:15 +0530

    crud app

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+  {
+    id: "1",
+    Name: "Manoj",
+    Age: "29",
+  },
+  {
+    id: "2",
+    Name: "Kartik",
+    Age: "30",
+  },
+  {
+    id: "3",
+    Name: "Guru",
+    Age: "25",
+  },
+];
+
+export default Employees;
